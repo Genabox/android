0ad002bc857599aeea1e405abb1386458332f005
Genabox <125927009+Genabox@users.noreply.github.com>
2023-12-09 12:39:11 +0200

    Update MainActivity.kt

--- alarm/kotlin/MainActivity.kt
+++ alarm/kotlin/MainActivity.kt
@@ -1,31 +1,108 @@
-package com.example.callboy2
+package com.example.callboy5
 
 import android.content.Context
-import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import java.util.*
-import java.util.Timer
-import java.util.TimerTask
-import android.media.MediaPlayer
-import android.os.Handler
-import android.os.Looper
-import android.os.PowerManager
+import kotlin.concurrent.schedule
 import android.app.Service
-import android.os.Build
+import android.content.Intent
+import android.os.IBinder
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
-import android.app.Notification
-import android.os.IBinder
+import android.os.Binder
+import android.os.PowerManager
+import androidx.core.app.NotificationCompat
 
+class MyAlarmService : Service() {
+    private lateinit var mediaPlayer: MediaPlayer
+    private val binder = LocalBinder()
+    private lateinit var wakeLock: PowerManager.WakeLock
 
+    inner class LocalBinder : Binder() {
+        fun getService(): MyAlarmService = this@MyAlarmService
+    }
 
+    override fun onCreate() {
+        super.onCreate()
+        mediaPlayer = MediaPlayer()
+        val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
+        wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK or PowerManager.ACQUIRE_CAUSES_WAKEUP, "AlarmService::MyWakelockTag")
+
+        wakeLock.acquire()
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val notification = createNotification()
+        startForeground(1, notification)
+
+        // Ваш код обработки срабатывания будильника и воспроизведения музыки
+
+        return START_NOT_STICKY
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.stop()
+        mediaPlayer.release()
+        wakeLock.release()
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return binder
+    }
+
+    private fun createNotification(): Notification {
+        val channelId = "AlarmServiceChannel"
+        val channelName = "Alarm Service Channel"
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val notificationBuilder = NotificationCompat.Builder(this, channelId)
+            .setContentTitle("Alarm Service")
+            .setContentText("Alarm Service is running in the background")
+            .setSmallIcon(R.drawable.ic_notification_icon) // Замените на свою иконку
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        return notificationBuilder.build()
+    }
+}
+
+class AlarmService : Service() {
+    private lateinit var mediaPlayer: MediaPlayer
+
+    override fun onCreate() {
+        super.onCreate()
+        mediaPlayer = MediaPlayer()
+        // Инициализация MediaPlayer и воспроизведение музыки
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        // Обработка срабатывания будильника, включение черного экрана и воспроизведение музыки
+        return START_NOT_STICKY
+    }
+
+    override fun onDestroy() {
+        // Остановка MediaPlayer и освобождение ресурсов
+        super.onDestroy()
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+}
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var mp3UrlEditText: EditText
     private lateinit var alarmTimeEditText: EditText
     private lateinit var alarmIntervalEditText: EditText
@@ -33,17 +110,15 @@ class MainActivity : AppCompatActivity() {
     private lateinit var startAlarmButton: Button
     private lateinit var stopAlarmButton: Button
     private lateinit var testAlarmButton: Button
-    private var alarmTimer: Timer? = null
-    private var backgroundThread: Thread? = null
-    private var mediaPlayer: MediaPlayer? = null
+    private lateinit var hideButton: Button
     private val handler = Handler()
-
+    private var mediaPlayer: MediaPlayer? = null
+    private var alarmTimer: Timer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Инициализация компонентов UI
         mp3UrlEditText = findViewById(R.id.mp3UrlEditText)
         alarmTimeEditText = findViewById(R.id.alarmTimeEditText)
         alarmIntervalEditText = findViewById(R.id.alarmIntervalEditText)
@@ -51,10 +126,8 @@ class MainActivity : AppCompatActivity() {
         startAlarmButton = findViewById(R.id.startAlarmButton)
         stopAlarmButton = findViewById(R.id.stopAlarmButton)
         testAlarmButton = findViewById(R.id.testAlarmButton)
-        startAlarmButton = findViewById(R.id.startAlarmButton)
-        startAlarmButton.setOnClickListener {
-            startAlarm()
-        }
+        hideButton = findViewById(R.id.hideButton)
+
         loadSettings()
 
         saveButton.setOnClickListener {
@@ -72,64 +145,19 @@ class MainActivity : AppCompatActivity() {
         testAlarmButton.setOnClickListener {
             testAlarm()
         }
+
+        hideButton.setOnClickListener {
+            hideAlarm()
+        }
+
+
+        // Установим начальное состояние кнопки "Стоп" в неактивное
+        stopAlarmButton.isEnabled = false
     }
-    class AlarmService : Service() {
-        private var mediaPlayer: MediaPlayer? = null
 
-        override fun onCreate() {
-            super.onCreate()
-            // Создание канала уведомлений для Android O и выше
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                val channel = NotificationChannel(
-                    "alarm_service_channel",
-                    "Alarm Service Channel",
-                    NotificationManager.IMPORTANCE_DEFAULT
-                )
-                val manager = getSystemService(NotificationManager::class.java)
-                manager.createNotificationChannel(channel)
-            }
-        }
-
-        override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-            val mp3Url = intent?.getStringExtra("mp3_url")
-
-            // Создание уведомления для Foreground Service
-            val notification = Notification.Builder(this, "alarm_service_channel")
-                .setContentTitle("Alarm Service")
-                .setContentText("Alarm is playing...")
-                .setSmallIcon(R.drawable.ic_alarm) // Замените на реальный идентификатор ресурса вашей иконки
-                .build()
-
-            startForeground(1, notification)
-
-            try {
-                mediaPlayer = MediaPlayer().apply {
-                    setDataSource(mp3Url)
-                    prepare()
-                    start()
-                }
-            } catch (e: Exception) {
-                e.printStackTrace()
-                stopSelf()
-            }
-
-            return START_NOT_STICKY
-        }
-
-        override fun onDestroy() {
-            super.onDestroy()
-            mediaPlayer?.stop()
-            mediaPlayer?.release()
-            mediaPlayer = null
-        }
-
-        override fun onBind(intent: Intent?): IBinder? {
-            return null
-        }
-    }
     private fun loadSettings() {
         val sharedPref = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
-        val mp3Url = sharedPref.getString("mp3_url", "https://file-examples.com/storage/febf69dcf3656dfd992b0fa/2017/11/file_example_MP3_700KB.mp3")
+        val mp3Url = sharedPref.getString("mp3_url", "https://samples-files.com/samples/Audio/mp3/sample-file-1.mp3")
         val alarmTime = sharedPref.getString("alarm_time", "21:41")
         val alarmInterval = sharedPref.getString("alarm_interval", "00:00:15")
 
@@ -153,6 +181,8 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "Настройки сохранены", Toast.LENGTH_SHORT).show()
     }
 
+    private var backgroundThread: Thread? = null
+
     private fun startAlarm() {
         val alarmTimeString = alarmTimeEditText.text.toString()
         val alarmTimeParts = alarmTimeString.split(":")
@@ -164,38 +194,32 @@ class MainActivity : AppCompatActivity() {
             if (hours != null && minutes != null && hours in 0..23 && minutes in 0..59) {
                 val currentTime = Calendar.getInstance()
 
-                val alarmTime = Calendar.getInstance().apply {
-                    set(Calendar.HOUR_OF_DAY, hours)
-                    set(Calendar.MINUTE, minutes)
-                    set(Calendar.SECOND, 0)
+                val alarmTime = Calendar.getInstance()
+                alarmTime.set(Calendar.HOUR_OF_DAY, hours)
+                alarmTime.set(Calendar.MINUTE, minutes)
+                alarmTime.set(Calendar.SECOND, 0)
 
-                    // Если указанное время уже прошло, добавляем 1 день
-                    if (before(currentTime)) {
-                        add(Calendar.DAY_OF_YEAR, 1)
-                    }
+                // Если указанное время уже прошло, добавляем 1 день
+                if (alarmTime.before(currentTime)) {
+                    alarmTime.add(Calendar.DAY_OF_YEAR, 1)
                 }
 
                 val delayMillis = alarmTime.timeInMillis - currentTime.timeInMillis
 
-                alarmTimer?.cancel() // Отменяем предыдущий таймер, если он существует
-                alarmTimer = Timer().apply {
-                    schedule(object : TimerTask() {
-                        override fun run() {
-                            handler.post {
-                                playAlarm()
-                            }
-                        }
-                    }, delayMillis)
+                alarmTimer = Timer()
+                alarmTimer?.schedule(delayMillis) {
+                    handler.post {
+                        playAlarm()
+                    }
                 }
 
                 startAlarmButton.isEnabled = false
                 testAlarmButton.isEnabled = false
                 stopAlarmButton.isEnabled = true
             } else {
-                Toast.makeText(this, "Введите корректное время", Toast.LENGTH_SHORT).show()
+                // Если время введено некорректно, можно добавить обработку ошибки или уведомление.
+                // Например, Toast.makeText(this, "Введите корректное время", Toast.LENGTH_SHORT).show()
             }
-        } else {
-            Toast.makeText(this, "Введите время в формате HH:mm", Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -203,48 +227,44 @@ class MainActivity : AppCompatActivity() {
         val mp3Url = mp3UrlEditText.text.toString()
 
         if (mp3Url.isNotEmpty()) {
-            val powerManager = getSystemService(Context.POWER_SERVICE) as PowerManager
-            val wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "MyApp::MyWakeLockTag").apply {
-                acquire(10*60*1000L /*10 minutes*/) // Захватываем WakeLock на 10 минут
-            }
-
             backgroundThread = Thread {
                 try {
                     mediaPlayer = MediaPlayer()
                     mediaPlayer?.setDataSource(mp3Url)
                     mediaPlayer?.prepare()
-                    mediaPlayer?.start()
 
                     mediaPlayer?.setOnCompletionListener {
                         handler.post {
-                            testAlarmButton.isEnabled = true
-                            startAlarmButton.isEnabled = true
-                            stopAlarmButton.isEnabled = false
+                            testAlarmButton.isEnabled = false
+                            startAlarmButton.isEnabled = false
+                            stopAlarmButton.isEnabled = true
+                        }
 
-                            val intervalString = alarmIntervalEditText.text.toString()
-                            val intervalParts = intervalString.split(":")
-                            if (intervalParts.size == 3) {
-                                val intervalHours = intervalParts[0].toLongOrNull()
-                                val intervalMinutes = intervalParts[1].toLongOrNull()
-                                val intervalSeconds = intervalParts[2].toLongOrNull()
+                        val intervalString = alarmIntervalEditText.text.toString()
+                        val intervalParts = intervalString.split(":")
+                        if (intervalParts.size == 3) {
+                            val intervalHours = intervalParts[0].toLongOrNull()
+                            val intervalMinutes = intervalParts[1].toLongOrNull()
+                            val intervalSeconds = intervalParts[2].toLongOrNull()
 
-                                if (intervalHours != null && intervalMinutes != null && intervalSeconds != null) {
-                                    val intervalMillis = intervalHours * 3600000 + intervalMinutes * 60000 + intervalSeconds * 1000
+                            if (intervalHours != null && intervalMinutes != null && intervalSeconds != null) {
+                                val intervalMillis =
+                                    intervalHours * 3600000 + intervalMinutes * 60000 + intervalSeconds * 1000
 
-                                    handler.postDelayed({
-                                        playAlarm()
-                                    }, intervalMillis)
-                                }
+                                handler.postDelayed({
+                                    playAlarm()
+                                }, intervalMillis)
                             }
                         }
                     }
+
+                    mediaPlayer?.start()
+                    backgroundThread = null
                 } catch (e: Exception) {
                     e.printStackTrace()
                     handler.post {
                         Toast.makeText(this, "Ошибка при проигрывании будильника", Toast.LENGTH_SHORT).show()
                     }
-                } finally {
-                    wakeLock.release() // Освобождаем WakeLock
                 }
             }
             backgroundThread?.start()
@@ -253,10 +273,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
-
     private fun stopAlarm() {
-        stopService(Intent(this, AlarmService::class.java))
         if (backgroundThread != null && backgroundThread?.isAlive == true) {
             backgroundThread?.interrupt()
         }
@@ -275,27 +292,67 @@ class MainActivity : AppCompatActivity() {
         stopAlarmButton.isEnabled = false
     }
 
+    private fun hideAlarm()
+    {
+        moveTaskToBack(true)
+    }
+
     private fun testAlarm() {
         val mp3Url = mp3UrlEditText.text.toString()
+        testAlarmButton.isEnabled = false
+        stopAlarmButton.isEnabled = true
+        startAlarmButton.isEnabled = false
+        if (mp3Url.isNotEmpty()) {
+            backgroundThread = Thread {
+                try {
+                    mediaPlayer = MediaPlayer()
+                    mediaPlayer?.setDataSource(mp3Url)
+                    mediaPlayer?.prepare()
+                    mediaPlayer?.start()
 
-        backgroundThread = Thread(Runnable {
-            mediaPlayer = MediaPlayer().apply {
-                setDataSource(mp3Url)
-                prepare()
-                start()
+                    // Включаем кнопку "Стоп"
+                    stopAlarmButton.isEnabled = true
+
+                    mediaPlayer?.setOnCompletionListener {
+                        handler.post {
+                            // Воспроизведение завершено, включаем кнопку "Тестировать будильник" и
+                            // отключаем кнопку "Стоп"
+                            testAlarmButton.isEnabled = true
+                            stopAlarmButton.isEnabled = false
+                            startAlarmButton.isEnabled = false
+                        }
+
+                        val intervalString = alarmIntervalEditText.text.toString()
+                        val intervalParts = intervalString.split(":")
+                        if (intervalParts.size == 3) {
+                            val intervalHours = intervalParts[0].toLongOrNull()
+                            val intervalMinutes = intervalParts[1].toLongOrNull()
+                            val intervalSeconds = intervalParts[2].toLongOrNull()
+
+                            if (intervalHours != null && intervalMinutes != null && intervalSeconds != null) {
+                                val intervalMillis =
+                                    intervalHours * 3600000 + intervalMinutes * 60000 + intervalSeconds * 1000
+
+                                handler.postDelayed({
+                                    playAlarm()
+                                }, intervalMillis)
+                            }
+                        }
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    handler.post {
+                        Toast.makeText(this, "Ошибка при тестировании будильника", Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
-            handler.post {
-                testAlarmButton.isEnabled = false
-                stopAlarmButton.isEnabled = true
-            }
-        }).apply {
-            start()
+            backgroundThread?.start()
+
+            // Отключаем кнопку "Тестировать будильник" на время воспроизведения
+            testAlarmButton.isEnabled = false
+        } else {
+            Toast.makeText(this, "Введите URL для тестирования", Toast.LENGTH_SHORT).show()
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        backgroundThread?.interrupt()
-        mediaPlayer?.release()
-    }
 }
